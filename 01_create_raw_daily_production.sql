0cad87c9e67e7ea01d931b2cce6662dfd71526c4
Aarthy Jeyakumar <jaarthy1997@gmail.com>
2026-06-10 18:46:21 -0600

    Update 01_create_raw_daily_production.sql

--- 01_create_raw_daily_production.sql
+++ 01_create_raw_daily_production.sql
@@ -1,35 +1,29 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2870
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl368\sa213\partightenfactor0
+-- Create raw staging table for Volve daily production dataset
+-- All fields imported as TEXT to avoid datatype and formatting issues during ingestion
 
-\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
-CREATE TABLE raw_daily_production (\
-\'a0\'a0\'a0 dateprd TEXT,\
-\'a0\'a0\'a0 well_bore_code TEXT,\
-\'a0\'a0\'a0 npd_well_bore_code TEXT,\
-\'a0\'a0\'a0 npd_well_bore_name TEXT,\
-\'a0\'a0\'a0 npd_field_code TEXT,\
-\'a0\'a0\'a0 npd_field_name TEXT,\
-\'a0\'a0\'a0 npd_facility_code TEXT,\
-\'a0\'a0\'a0 npd_facility_name TEXT,\
-\'a0\'a0\'a0 on_stream_hrs TEXT,\
-\'a0\'a0\'a0 avg_downhole_pressure TEXT,\
-\'a0\'a0\'a0 avg_downhole_temperature TEXT,\
-\'a0\'a0\'a0 avg_dp_tubing TEXT,\
-\'a0\'a0\'a0 avg_annulus_press TEXT,\
-\'a0\'a0\'a0 avg_choke_size_p TEXT,\
-\'a0\'a0\'a0 avg_choke_uom TEXT,\
-\'a0\'a0\'a0 avg_whp_p TEXT,\
-\'a0\'a0\'a0 avg_wht_p TEXT,\
-\'a0\'a0\'a0 dp_choke_size TEXT,\
-\'a0\'a0\'a0 bore_oil_vol TEXT,\
-\'a0\'a0\'a0 bore_gas_vol TEXT,\
-\'a0\'a0\'a0 bore_wat_vol TEXT,\
-\'a0\'a0\'a0 bore_wi_vol TEXT,\
-\'a0\'a0\'a0 flow_kind TEXT,\
-\'a0\'a0\'a0 well_type TEXT\
-);}
+CREATE TABLE raw_daily_production (
+    dateprd TEXT,
+    well_bore_code TEXT,
+    npd_well_bore_code TEXT,
+    npd_well_bore_name TEXT,
+    npd_field_code TEXT,
+    npd_field_name TEXT,
+    npd_facility_code TEXT,
+    npd_facility_name TEXT,
+    on_stream_hrs TEXT,
+    avg_downhole_pressure TEXT,
+    avg_downhole_temperature TEXT,
+    avg_dp_tubing TEXT,
+    avg_annulus_press TEXT,
+    avg_choke_size_p TEXT,
+    avg_choke_uom TEXT,
+    avg_whp_p TEXT,
+    avg_wht_p TEXT,
+    dp_choke_size TEXT,
+    bore_oil_vol TEXT,
+    bore_gas_vol TEXT,
+    bore_wat_vol TEXT,
+    bore_wi_vol TEXT,
+    flow_kind TEXT,
+    well_type TEXT
+);
