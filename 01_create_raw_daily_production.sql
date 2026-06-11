47cb0c0d36483c12a0e0638910ec9620bdd73907
Aarthy Jeyakumar <jaarthy1997@gmail.com>
2026-06-10 18:45:20 -0600

    Add files via upload

---
+++ 01_create_raw_daily_production.sql
@@ -0,0 +1,35 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl368\sa213\partightenfactor0
+
+\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
+CREATE TABLE raw_daily_production (\
+\'a0\'a0\'a0 dateprd TEXT,\
+\'a0\'a0\'a0 well_bore_code TEXT,\
+\'a0\'a0\'a0 npd_well_bore_code TEXT,\
+\'a0\'a0\'a0 npd_well_bore_name TEXT,\
+\'a0\'a0\'a0 npd_field_code TEXT,\
+\'a0\'a0\'a0 npd_field_name TEXT,\
+\'a0\'a0\'a0 npd_facility_code TEXT,\
+\'a0\'a0\'a0 npd_facility_name TEXT,\
+\'a0\'a0\'a0 on_stream_hrs TEXT,\
+\'a0\'a0\'a0 avg_downhole_pressure TEXT,\
+\'a0\'a0\'a0 avg_downhole_temperature TEXT,\
+\'a0\'a0\'a0 avg_dp_tubing TEXT,\
+\'a0\'a0\'a0 avg_annulus_press TEXT,\
+\'a0\'a0\'a0 avg_choke_size_p TEXT,\
+\'a0\'a0\'a0 avg_choke_uom TEXT,\
+\'a0\'a0\'a0 avg_whp_p TEXT,\
+\'a0\'a0\'a0 avg_wht_p TEXT,\
+\'a0\'a0\'a0 dp_choke_size TEXT,\
+\'a0\'a0\'a0 bore_oil_vol TEXT,\
+\'a0\'a0\'a0 bore_gas_vol TEXT,\
+\'a0\'a0\'a0 bore_wat_vol TEXT,\
+\'a0\'a0\'a0 bore_wi_vol TEXT,\
+\'a0\'a0\'a0 flow_kind TEXT,\
+\'a0\'a0\'a0 well_type TEXT\
+);}
